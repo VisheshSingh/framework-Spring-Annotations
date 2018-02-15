4bbd57ed3aa5439f8fec169b81fd65b7dbab38af
Vishesh <visheshsingh16@gmail.com>
2018-02-14 21:33:03 -0600

    Bean Lifecycle method annotations

--- src/main/java/com/vishesh/framework_springanno/TennisCoach.java
+++ src/main/java/com/vishesh/framework_springanno/TennisCoach.java
@@ -1,12 +1,13 @@
 package com.vishesh.framework_springanno;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -15,6 +16,17 @@ public class TennisCoach implements Coach {
 
 	public TennisCoach() {
 		System.out.println(">> Tennis Coach: Inside default constructor");
+	}
+
+	// define init and destroy methods
+	@PostConstruct
+	public void doStart() {
+		System.out.println(">> Inside Startup stuff: Before Bean is called");
+	}
+
+	@PreDestroy
+	public void doDestroy() {
+		System.out.println(">> Inside Destroy stuff: After Bean is destroyed");
 	}
 
 	/*
